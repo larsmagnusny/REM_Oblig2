8aba5f5d948110caf868ea6f153ed2e86abf600e
LAPTOP-3PAIDASL\larsm <lars.magnus.nyland@gmail.com>
2017-02-15 14:47:35 +0100

    C++ Class for the Character

---
+++ REMOblig2/Source/REMOblig2/MainCharacter.cpp
@@ -0,0 +1,35 @@
+// REM_Prototype Copyright (C) 2017 (Lars Magnus Nyland & Une Johnsen)
+
+#include "REMOblig2.h"
+#include "MainCharacter.h"
+
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
