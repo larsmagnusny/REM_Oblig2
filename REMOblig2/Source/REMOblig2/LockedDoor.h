56b6fbc0bbd8b7363adff20c0f79ee85415f2684
DESKTOP-53H1DBN\larsm <lars.magnus.nyland@gmail.com>
2017-02-20 22:00:30 +0100

    Added the Main Character to the level, Scripts to the door, postprocessing volume and Navmesh.

--- REMOblig2/Source/REMOblig2/LockedDoor.h
+++ REMOblig2/Source/REMOblig2/LockedDoor.h
@@ -19,7 +19,7 @@ enum class OpenCondition : uint8
 	OPEN_NORMAL UMETA(DisplayName = "Door Opens no matter what")
 };
 
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class REMOBLIG2_API ULockedDoor : public UInteractableComponent
 {
 	GENERATED_BODY()
